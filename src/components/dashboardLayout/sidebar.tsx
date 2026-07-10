4264db2d237fb5d0612efbf68b62076c7ffa216e
unknown <ahaseko1@gmail.com>
2026-07-10 08:42:18 +0700

    fix update ttd

--- src/components/dashboardLayout/sidebar.tsx
+++ src/components/dashboardLayout/sidebar.tsx
@@ -1,101 +1,109 @@
-import { Avatar, Divider, Grid } from '@nextui-org/react';
-import { RiAdminFill } from 'react-icons/ri'
-import { BiLogOut, BiLogOutCircle } from 'react-icons/bi'
-import { AiFillHome, AiFillSetting } from 'react-icons/ai'
-import { HiUsers } from 'react-icons/hi'
-import { useRouter } from 'next/router';
-import Link from 'next/link';
-import { Fragment, useEffect, useState } from 'react';
-import Cookies from 'js-cookie';
-import axios from 'axios';
-import { Image } from 'antd';
+import { Avatar, Divider, Grid } from "@nextui-org/react";
+import { RiAdminFill } from "react-icons/ri";
+import { BiLogOut, BiLogOutCircle } from "react-icons/bi";
+import { AiFillHome, AiFillSetting } from "react-icons/ai";
+import { HiUsers } from "react-icons/hi";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { Fragment, useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import axios from "axios";
+import { Image } from "antd";
 
 const ListMenu = [
-    {
-        label: 'Home',
-        link: '/dashboard',
-        icon: <AiFillHome className='text-white text-xl' />
-    },
-    {
-        label: 'Anggota',
-        link: '/dashboard/anggota',
-        icon: <HiUsers className='text-white text-xl' />
-    },
-    {
-        label: 'Setting',
-        link: '/dashboard/setting',
-        icon: <AiFillSetting className='text-white text-xl' />
-    }
-]
+  {
+    label: "Home",
+    link: "/dashboard",
+    icon: <AiFillHome className="text-white text-xl" />,
+  },
+  {
+    label: "Anggota",
+    link: "/dashboard/anggota",
+    icon: <HiUsers className="text-white text-xl" />,
+  },
+  {
+    label: "Setting",
+    link: "/dashboard/setting",
+    icon: <AiFillSetting className="text-white text-xl" />,
+  },
+];
 
-export default function Sidebar({ Logout }){
+export default function Sidebar({ Logout }) {
+  const router = useRouter();
 
-    const router = useRouter()
-    
-    const [ users, setUsers ] = useState({
-        username: '',
-        profile_picture: ''
-    })
-    useEffect(() => {
-        setUsers({ 
-            username: Cookies.get('username'),
-            profile_picture: Cookies.get('profile_picture') 
-        })
-    },[])
-    
-    return (
-        <div className="bg-blue-secondary min-h-screen relative shadow-lg hidden lg:block col-span-2">
-            <div className='py-10 flex flex-col items-center justify-center'>
-                <div className='w-24 h-24 rounded-full overflow-hidden'>
-                    <Image 
-                        preview={false}
-                        src={users?.profile_picture}
-                    />
-                </div>
-                <p className='text-white mt-2 font-semibold'>{users.username}</p>
+  const [users, setUsers] = useState({
+    username: "",
+    profile_picture: "",
+  });
+  useEffect(() => {
+    setUsers({
+      username: Cookies.get("username"),
+      profile_picture: Cookies.get("profile_picture"),
+    });
+  }, []);
+
+  return (
+    <div className="bg-blue-secondary min-h-screen relative shadow-lg hidden lg:block col-span-2">
+      <div className="py-10 flex flex-col items-center justify-center">
+        <div className="w-24 h-24 rounded-full overflow-hidden">
+          <Image preview={false} src={users?.profile_picture} width={200} />
+          {/* <Avatar src={users?.profile_picture} /> */}
+        </div>
+        <p className="text-white mt-2 font-semibold">{users.username}</p>
+      </div>
+
+      <Divider className="bg-gray-500 h-.5 mb-2" />
+
+      <div>
+        {ListMenu?.map((item, idx) => (
+          <Link href={item?.link} key={idx}>
+            <div
+              className={`py-3 px-5 cursor-pointer flex items-center space-x-4 ${
+                router.pathname === item?.link
+                  ? "bg-blue-darkSecondary"
+                  : "hover:bg-blue-darkSecondary"
+              }`}
+            >
+              {item?.icon}
+              <div className="flex items-center justify-center">
+                <p className="text-white my-auto">{item?.label}</p>
+              </div>
             </div>
+          </Link>
+        ))}
+      </div>
 
-            <Divider className='bg-gray-500 h-.5 mb-2'  />
-
-            <div>
-                {ListMenu?.map((item, idx) => (
-                    <Link href={item?.link} key={idx}>
-                        <div className={`py-3 px-5 cursor-pointer flex items-center space-x-4 ${router.pathname === item?.link ? 'bg-blue-darkSecondary' : 'hover:bg-blue-darkSecondary'}`}>
-                            {item?.icon}
-                            <div className='flex items-center justify-center'>
-                                <p className='text-white my-auto'>{item?.label}</p>
-                            </div>
-                        </div>
-                    </Link>
-                ))}
-            </div>
-
-            {/* <div className='absolute bottom-0 w-full'>
+      {/* <div className='absolute bottom-0 w-full'>
                 <div className={`py-3 px-5 mx-0.5 cursor-pointer flex items-center justify-center space-x-4 border border-darkSecondary`} onClick={Logout}>
                     <BiLogOut className='text-white text-xl' />
                     <p className='text-white my-auto'>Logout</p>
                 </div>
             </div> */}
-
-        </div>
-    )
+    </div>
+  );
 }
 
-export function SidebarMobile(){
-    
-    const router = useRouter()
+export function SidebarMobile() {
+  const router = useRouter();
 
-    return (
-        <Fragment>
-            <div className="absolute w-screen h-screen bg-gray-800 bg-opacity-80 flex flex-col items-center justify-center" style={{ zIndex: 1400 }}>
-                {ListMenu?.map((item, idx) => (
-                    <Link href={item?.link} key={idx}>
-                        <div className={`py-3 px-5 cursor-pointer flex items-center space-x-4`}>
-                            <p className={`text-white font-semibold text-2xl`}>{item?.label}</p>
-                        </div>
-                    </Link>
-                ))}           
+  return (
+    <Fragment>
+      <div
+        className="absolute w-screen h-screen bg-gray-800 bg-opacity-80 flex flex-col items-center justify-center"
+        style={{ zIndex: 1400 }}
+      >
+        {ListMenu?.map((item, idx) => (
+          <Link href={item?.link} key={idx}>
+            <div
+              className={`py-3 px-5 cursor-pointer flex items-center space-x-4`}
+            >
+              <p className={`text-white font-semibold text-2xl`}>
+                {item?.label}
+              </p>
             </div>
-        </Fragment>
-    )
+          </Link>
+        ))}
+      </div>
+    </Fragment>
+  );
 }
--- src/templates/auth/loginToko.tsx
+++ src/templates/auth/loginToko.tsx
@@ -1,78 +1,89 @@
 import { Fragment, useEffect } from "react";
 import React, { useState } from "react";
 import Image from "next/image";
-import LogoMerchant from "@base/src/asset/img/merchant.png"
+import LogoMerchant from "@base/src/asset/img/merchant.svg";
 import { Input, Button, Loading } from "@nextui-org/react";
-import Axios from 'axios'
+import Axios from "axios";
 import Cookies from "js-cookie";
 import { toast } from "react-toastify";
 
-export default function LoginDefaultPageToko(){
+export default function LoginDefaultPageToko() {
+  const [form, setForm] = useState({
+    ID: "",
+  });
 
-    const [ form, setForm ] = useState({
-        ID: '',
-    })
+  const [loadingSubmit, setLoadingSubmit] = useState(false);
 
-    const [ loadingSubmit, setLoadingSubmit ] = useState(false)
+  const Submit = async (e: any) => {
+    e.preventDefault();
+    setLoadingSubmit(true);
+    Axios.post("/api/toko/login", form)
+      .then((res) => {
+        const { token } = res?.data;
+        const { alamat, email, nama, noTelp, status, toko } = res?.data?._doc;
+        console.log(res?.data);
+        Cookies.set("token", token);
+        Cookies.set("alamat", alamat);
+        Cookies.set("email", email);
+        Cookies.set("nama", nama);
+        Cookies.set("noTelp", noTelp);
+        Cookies.set("toko", toko);
 
-    const Submit = async (e:any) => {
-        e.preventDefault()
-        setLoadingSubmit(true)
-        Axios.post('/api/toko/login', form)
-            .then(res => {
-                const { token } = res?.data
-                const { alamat, email, nama, noTelp, status, toko } = res?.data?._doc
-                console.log(res?.data)
-                Cookies.set('token', token)
-                Cookies.set('alamat', alamat)
-                Cookies.set('email', email)
-                Cookies.set('nama', nama)
-                Cookies.set('noTelp', noTelp)
-                Cookies.set('toko', toko)
-                
-                window.location.href= '/toko/dashboard'
-            })
-            .catch(err => {
-                toast.error(err?.response?.data?.msg, { position: toast.POSITION.TOP_CENTER })
-            }).finally(() => {
-                setLoadingSubmit(false)
-            })
-    }
+        window.location.href = "/toko/dashboard";
+      })
+      .catch((err) => {
+        toast.error(err?.response?.data?.msg, {
+          position: toast.POSITION.TOP_CENTER,
+        });
+      })
+      .finally(() => {
+        setLoadingSubmit(false);
+      });
+  };
 
-
-    return (
-        <div className="grid grid-cols-1 lg:grid-cols-2 min-h-screen">
-            <div className="bg-gradient-to-br from-primaryToko to-red-500 hidden lg:flex flex-col item-center justify-center">
-                <p className="text-3xl text-center text-white">Kuliner Taman Harapan Baru</p>
-                <div className="mx-auto mt-10">
-                    <Image 
-                        alt=""
-                        src={LogoMerchant}
-                    />
-                </div>
-            </div>
-            <div className="flex flex-col items-center justify-center">
-                <div className="blur-lg lg:hidden absolute bg-gradient-to-br from-primaryToko to-red-500 w-screen h-screen flex items-center justify-center">
-                    <Image alt='' src={LogoMerchant} />
-                </div>
-                
-                <form className="my-20 w-10/12 lg:w-1/2 space-y-10 bg-white z-20 p-10 rounded" onSubmit={Submit}>
-                    <div className="mb-5">
-                        <p className="text-2xl font-semibold text-center">Selamat Datang</p>
-                        <p className="text-sm text-center">Tolong isikan UNIQUE ID anda untuk masuk</p>
-                    </div>
-                    <Input 
-                        onChange={e => setForm({ ...form, ID: e.target.value })} 
-                        name="UniqID" 
-                        fullWidth 
-                        underlined 
-                        labelPlaceholder="Unique ID" 
-                    />
-                    <Button type='submit' style={{ width: '100%' }}>
-                        {loadingSubmit ? <Loading size='sm' color='white' /> : 'Submit' }
-                    </Button>
-                </form>
-            </div>
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 min-h-screen">
+      <div className="bg-gradient-to-br from-primaryToko to-red-500 hidden lg:flex flex-col item-center justify-center">
+        <p className="text-3xl text-center text-white">
+          Kuliner Taman Harapan Baru
+        </p>
+        <div className="mx-auto mt-10">
+          <Image alt="" src={LogoMerchant} />
         </div>
-    )
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <div className="blur-lg lg:hidden absolute bg-gradient-to-br from-primaryToko to-red-500 w-screen h-screen flex items-center justify-center">
+          <Image alt="" src={LogoMerchant} />
+        </div>
+
+        <form
+          className="my-20 w-10/12 lg:w-1/2 space-y-10 bg-white z-20 p-10 rounded"
+          onSubmit={Submit}
+        >
+          <div className="mb-5">
+            <p className="text-2xl font-semibold text-center">Selamat Datang</p>
+            <p className="text-sm text-center">
+              Tolong isikan UNIQUE ID anda untuk masuk
+            </p>
+          </div>
+          <Input
+            onChange={(e) => setForm({ ...form, ID: e.target.value })}
+            name="UniqID"
+            fullWidth
+            underlined
+            labelPlaceholder="Unique ID"
+          />
+          <Button type="submit" style={{ width: "100%" }}>
+            {loadingSubmit ? <Loading size="sm" color="white" /> : "Submit"}
+          </Button>
+          <div className="space-y-0.5">
+            <p className="text-lg bold text-center">Demo ? use this</p>
+            <p className="text-lg bold text-center">
+              UniqID: 6a503fc2e1e1b71b7734de14
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
